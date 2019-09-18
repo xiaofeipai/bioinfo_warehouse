8a4649bbdeda0b5a4b77507723382084b7501a8c
xiaofeipai <maplezheng@hust.edu.cn>
2019-09-18 13:55:43 +0800

    complete matrix

--- matrix/mymatirx.cpp
+++ matrix/mymatirx.cpp
@@ -1,15 +1,37 @@
 class Mymatrix{
-    unsigned int row, col;
-    double** array;
+    private:
+        unsigned int row, col;
+        double** array;
+    public:
+        void change_matrix_value(double v, int i, int j);
+        double getvalue(int i, int j);
+        Mymatrix();
+        Mymatrix(unsigned int row, unsigned int col);
+        ~Mymatrix();
+}
 
-    Mymatrix(){
+
+void Mymatrix::change_matrix_value(double v, int i, int j){
+    if(i < row&&j < col){
+        array[i][j] = v;
+    }
+}
+
+double Mymatrix::getvalue(int i, int j){
+    if(i < row&&j < col){
+        return array[i][j];
+    }
+}
+
+Mymatrix::Mymatrix(){
         row = 0;
         col = 0;
         **array = new double*[1];
         array[0] = new double[1];
         array[0][0] = 0;
-    }
-    Mymatrix(unsigned int row, unsigned int col){
+}
+
+Mymatrix::Mymatrix(unsigned int row, unsigned int col){
         **array = new double*[row];
         for(int i = 0; i != row; i++){
             array[i] = new double[col];
@@ -18,5 +40,11 @@ class Mymatrix{
             for(int j = 0; j != col; j++)
                 array[i][j] = 0.0;
         }
+}
+
+Mymatrix::~ ~Mymatrix(){
+    for(int i = 0; i!= row; i++){
+        delete []array[i];
     }
+    delete []array;
 }
